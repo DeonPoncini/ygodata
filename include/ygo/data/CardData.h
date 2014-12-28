e2890b14e7047a52bcbfae952a1b39a4bfa6491e
Deon Poncini <dex1337@gmail.com>
2014-12-27 17:19:44 -0800

    Add C friendly CardData structure

--- include/ygo/data/CardData.h
+++ include/ygo/data/CardData.h
@@ -2,14 +2,14 @@
 #define YGO_DATA_CARDDATA_H
 
 #include <string>
-#include <zephyr/CExport.h>
 
 #include "CardType.h"
 
-Z_NS_START(ygo)
-Z_NS_START(data)
+namespace ygo
+{
+namespace data
+{
 
-#ifdef __cplusplus
 struct StaticCardData
 {
     std::string name;
@@ -30,30 +30,8 @@ struct StaticCardData
     // card text
     std::string text;
 };
-#endif
 
-struct ZD(ygo)ZD(data)C_StaticCardData
-{
-    const char* name;
-    ZU(ygo)ZU(data)CardType cardType;
-    // monster only
-    ZU(ygo)ZU(data)Attribute attribute;
-    ZU(ygo)ZU(data)MonsterType monsterType;
-    ZU(ygo)ZU(data)Type type;
-    ZU(ygo)ZU(data)MonsterType monsterAbility;
-    int level;
-    int attack;
-    int defense;
-    int lpendulum;
-    int rpendulum;
-    // spell and trap only
-    ZU(ygo)ZU(data)SpellType spellType;
-    ZU(ygo)ZU(data)TrapType trapType;
-    // card text
-    const char* text;
-};
-
-Z_NS_END
-Z_NS_END
+}
+}
 
 #endif
